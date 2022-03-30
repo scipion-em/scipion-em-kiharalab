862bfc6ef40c27a300418ce00d148d0eee86fb71
Martin Salinas <martin.salinas@estudiante.uam.es>
2022-03-30 04:59:20 +0200

    Fixed installation process for enviroment creation step

--- kiharalab/__init__.py
+++ kiharalab/__init__.py
@@ -35,15 +35,16 @@ _references = ['genki2021DAQ']
 
 class Plugin(pwem.Plugin):
     """
-    Definition of class variables.
-    For each protocol, a variable of type _<protocolName>Home and _<protocolName>Repo will be created
-    _<protocolNameInLowercase>Home will be a folder located inside EM_ROOT and its name will be <protocolNameInUppercase>-<protocolVersion>. For example, "DAQ-1.0"
+    Definition of class variables. For each protocol, three variables will be created.
+    <protocolNameInUppercase>_WITH_VERSION will contain <protocolNameInUppercase>-<protocolVersion>. For example, "DAQ-1.0"
+    _<protocolNameInLowercase>Home will contain the full path of the protocol, ending with a folder whose name will be <protocolNameInUppercase>_WITH_VERSION variable. For example: "~/Documents/scipion/software/em/DAQ-1.0"
     _<protocolNameInLowercase>Repo will be a folder inside _<protocolNameInLowercase>Home and its name will be <protocolNameInUppercase>. For example: "DAQ"
     """
     names = [name.upper() for name in PROTOCOL_LIST]
     for name in names:
         nameInLowercase = name.lower()
-        locals()["_" + nameInLowercase + "Home"] = os.path.join(pwem.Config.EM_ROOT, globals()[name] + '-' + globals()[name + "_DEFAULT_VERSION"])
+        locals()[name + "_WITH_VERSION"] = name + '-' + globals()[name + "_DEFAULT_VERSION"]
+        locals()["_" + nameInLowercase + "Home"] = os.path.join(pwem.Config.EM_ROOT, locals()[name + "_WITH_VERSION"])
         locals()["_" + nameInLowercase + "Repo"] = os.path.join(locals()["_" + nameInLowercase + "Home"], globals()[name])
     print("CLASS DEFINITION - ") # REMOVE
 
@@ -57,9 +58,9 @@ class Plugin(pwem.Plugin):
         <protocolNameInUppercase>_ENV will contain the name of the conda enviroment for that protocol. For example: "DAQ-1.0"
         """
         for name in cls.names:
-            protocolHomeAndEnv = globals()[name] + "-" + globals()[name + "_DEFAULT_VERSION"]
+            protocolHomeAndEnv = name + '-' + globals()[name + "_DEFAULT_VERSION"]
             cls._defineEmVar(globals()[name + "_HOME"], protocolHomeAndEnv)
-            cls._defineEmVar(name + "_ENV", protocolHomeAndEnv)
+            cls._defineVar(name + "_ENV", protocolHomeAndEnv)
         print("CUSTOM PRINT - end - _defineVariables") # REMOVE
 
     @classmethod
@@ -71,42 +72,38 @@ class Plugin(pwem.Plugin):
         for name in cls.names:
             nameInLowercase = name.lower()
             cls.addProtocolPackage(env,
-                                    name,
+                                    globals()[name],
                                     getattr(cls, "_" + nameInLowercase + "Home"),
                                     getattr(cls, "_" + nameInLowercase + "Repo"),
-                                    globals()[name],
+                                    name,
                                     globals()[name + "_DEFAULT_VERSION"])
         print("CUSTOM PRINT - end - defineBinaries") # REMOVE
 
     @classmethod
-    def addProtocolPackage(cls, env, protocolName, protocolHome, protocolRepo, folderName, protocolVersion):
-        print("Params:")
-        print("Name: ", protocolName)
-        print("Home: ", protocolHome)
-        print("Repo :", protocolRepo)
-        print("folderName: ", folderName)
-        print("Version :", protocolVersion)
-        return;
-        exit()
-        print("CUSTOM PRINT - start - addProtocolPackage") # REMOVE
-        installationCmd = 'cd {} && git clone {} && '.format(protocolHome, cls.getGitUrl(protocolName))
-        installationCmd += '{} && conda create -y -n {}-env python=3.8.5 && {} && ' \
-            .format(cls.getCondaActivationCmd(), protocolName.lower(), cls.getEnvActivation(protocolName))
-        installationCmd += 'cd {} && pip install -r requirement.txt && '.format(protocolRepo)
+    def addProtocolPackage(cls, env, protocolName, protocolHome, protocolRepo, protocolVariableName, protocolVersion):
+        """
+        Define and execute commands for protocol installation.
+        Every command has it's own checkpoint so if, for some reason, process is interrupted, only commands that were not completed will be repeated.
+        cloneCmd clones the repo in the right folder.
+        envCreationCmd creates the conda enviroment and installs the required packages
+        """
+        cloneCmd = 'cd {} && git clone {} && touch REPO_CLONED' .format(protocolHome, cls.getGitUrl(protocolName))
+        envCreationCmd = '{} conda create -y -n {} python={} && {} && cd {} && conda install pip && $CONDA_PREFIX/bin/pip install -r requirements.txt && cd .. && touch ENVIROMENT_CREATED'\
+            .format(cls.getCondaActivationCmd(),
+                    getattr(cls, protocolVariableName + "_WITH_VERSION"),
+                    globals()[protocolVariableName + "_PYTHON_VERSION"],
+                    cls.getProtocolActivationCommand(protocolVariableName),
+                    protocolRepo)
+        print("Clone command: ", cloneCmd) # REMOVE
+        print("Env creation command: ", envCreationCmd) # REMOVE
 
-        # Creating validation file
-        PROTOCOL_INSTALLED = '%s_installed' % folderName
-        installationCmd += 'cd .. && touch %s' % PROTOCOL_INSTALLED  # Flag installation finished
-        print("CUSTOM PRINT - middle - addProtocolPackage - installationCmd") # REMOVE
-        print(installationCmd) # REMOVE
-
-        env.addPackage(folderName,
+        env.addPackage(protocolVariableName,
                        version=protocolVersion,
                        tar='void.tgz',
-                       commands=[(installationCmd, PROTOCOL_INSTALLED)],
+                       commands=[(cloneCmd, "REPO_CLONED"), (envCreationCmd, "ENVIROMENT_CREATED")],
                        neededProgs=["conda", "pip"],
                        default=True)
-        print("CUSTOM PRINT - end - addProtocolPackage")
+        print("CUSTOM PRINT - end - addProtocolPackage") # REMOVE
 
     @classmethod
     def runDAQ(cls, protocol, args, outDir=None, clean=True):
@@ -159,12 +156,18 @@ class Plugin(pwem.Plugin):
     
     @classmethod
     def getGitUrl(cls, protocolName):
+        """
+        Returns the GitHub url for the given Kiharalab protocol
+        """
         print("CUSTOM PRINT - getGitUrl ", protocolName) # REMOVE
         return KIHARALAB_GIT + protocolName
     
     @classmethod
-    def getProtocolActivationCommand(cls, protocolName):
-        pass
+    def getProtocolActivationCommand(cls, variableName):
+        """
+        Returns the conda activation command for the given protocol.
+        """
+        return "conda activate " + getattr(cls, variableName + "_WITH_VERSION")
 
     # ---------------------------------- Utils functions  -----------------------
 
