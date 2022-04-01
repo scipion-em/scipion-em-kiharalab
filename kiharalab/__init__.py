f2771b5d11ee604a2627c2b6a6b374eda9200320
Martin Salinas <ssalinasmartin@gmail.com>
2022-04-01 06:30:02 +0200

    Finished installation process
    Installation process now fully works. Some lines are still waiting por a PR to be merged.

--- kiharalab/__init__.py
+++ kiharalab/__init__.py
@@ -46,11 +46,9 @@ class Plugin(pwem.Plugin):
         locals()[name + "_WITH_VERSION"] = name + '-' + globals()[name + "_DEFAULT_VERSION"]
         locals()["_" + nameInLowercase + "Home"] = os.path.join(pwem.Config.EM_ROOT, locals()[name + "_WITH_VERSION"])
         locals()["_" + nameInLowercase + "Repo"] = os.path.join(locals()["_" + nameInLowercase + "Home"], globals()[name])
-    print("CLASS DEFINITION - ") # REMOVE
 
     @classmethod
     def _defineVariables(cls):
-        print("CUSTOM PRINT - start - _defineVariables") # REMOVE
         """
         Return and write a home and conda enviroment variable in the config file.
         Each protocol will have a variable called <protocolNameInUppercase>_HOME, and another called <protocolNameInUppercase>_ENV
@@ -61,14 +59,12 @@ class Plugin(pwem.Plugin):
             protocolHomeAndEnv = name + '-' + globals()[name + "_DEFAULT_VERSION"]
             cls._defineEmVar(globals()[name + "_HOME"], protocolHomeAndEnv)
             cls._defineVar(name + "_ENV", protocolHomeAndEnv)
-        print("CUSTOM PRINT - end - _defineVariables") # REMOVE
 
     @classmethod
     def defineBinaries(cls, env):
         """
         This function defines the binaries for each protocol.
         """
-        print("CUSTOM PRINT - start - defineBinaries") # REMOVE
         for name in cls.names:
             nameInLowercase = name.lower()
             cls.addProtocolPackage(env,
@@ -77,7 +73,6 @@ class Plugin(pwem.Plugin):
                                     getattr(cls, "_" + nameInLowercase + "Repo"),
                                     name,
                                     globals()[name + "_DEFAULT_VERSION"])
-        print("CUSTOM PRINT - end - defineBinaries") # REMOVE
 
     @classmethod
     def addProtocolPackage(cls, env, protocolName, protocolHome, protocolRepo, protocolVariableName, protocolVersion):
@@ -85,7 +80,7 @@ class Plugin(pwem.Plugin):
         Define and execute commands for protocol installation.
         Every command has it's own checkpoint so if, for some reason, process is interrupted, only commands that were not completed will be repeated.
         cloneCmd clones the repo in the right folder.
-        envCreationCmd creates the conda enviroment and installs the required packages
+        envCreationCmd creates the conda enviroment and installs the required python packages.
         """
         cloneCmd = 'cd {} && git clone {} && touch REPO_CLONED' .format(protocolHome, cls.getGitUrl(protocolName))
         envCreationCmd = '{} conda create -y -n {} python={} && {} && cd {} && conda install pip && $CONDA_PREFIX/bin/pip install -r requirements.txt && cd .. && touch ENVIROMENT_CREATED'\
@@ -94,16 +89,51 @@ class Plugin(pwem.Plugin):
                     globals()[protocolVariableName + "_PYTHON_VERSION"],
                     cls.getProtocolActivationCommand(protocolVariableName),
                     protocolRepo)
-        print("Clone command: ", cloneCmd) # REMOVE
-        print("Env creation command: ", envCreationCmd) # REMOVE
+        
+        ## REMOVE LATER ##
+        if protocolVariableName == 'EMAP2SEC': # REMOVE WHEN PR APPROVED
+            envCreationCmd = '{} conda create -y -n {} python={} && {} && cd {} && conda install pip && echo \'tensorflow==1.15\\nscikit-learn==0.24.2\\npandas==1.1.5\\nnumpy==1.16.4\' > requirements.txt && $CONDA_PREFIX/bin/pip install -r requirements.txt && cd .. && touch ENVIROMENT_CREATED'\
+                .format(cls.getCondaActivationCmd(),
+                        getattr(cls, protocolVariableName + "_WITH_VERSION"),
+                        globals()[protocolVariableName + "_PYTHON_VERSION"],
+                        cls.getProtocolActivationCommand(protocolVariableName),
+                        protocolRepo)
+        ## REMOVE LATER ##
+
+        commandList = [(cloneCmd, "REPO_CLONED"), (envCreationCmd, "ENVIROMENT_CREATED")]
+        
+        # Check if protocol has extra files to download
+        extraFilesVariableName = protocolVariableName + "_EXTRA_FILES"
+        if (extraFilesVariableName in globals()):
+            # Add all extra files as separate commands to create one checkpoint per file
+            downloadCommandList = cls.getProtocolExtraFiles(globals()[extraFilesVariableName])
+            for i in range(len(downloadCommandList)):
+                checkpointName = "EXTRA_FILE_" + str(i)
+                commandList.append(("cd " + protocolRepo + " && " + downloadCommandList[i] + " && cd " + protocolHome + " && touch " + checkpointName, checkpointName))
 
         env.addPackage(protocolVariableName,
                        version=protocolVersion,
                        tar='void.tgz',
-                       commands=[(cloneCmd, "REPO_CLONED"), (envCreationCmd, "ENVIROMENT_CREATED")],
+                       commands=commandList,
                        neededProgs=["conda", "pip"],
                        default=True)
-        print("CUSTOM PRINT - end - addProtocolPackage") # REMOVE
+    
+    @classmethod
+    def getProtocolExtraFiles(cls, files):
+        """
+        Creates the neccessary commands to download the extra required files for the protocol.
+        -O flag for wget is used to overwrite the downloaded file if one with the same name exists.
+        This is done to overwrite potential corrupt files whose download was not fully completed
+        """
+        commandList = []
+        for file in files:
+            # Getting filename and path for wget
+            fileName = os.popen("basename " + file[0]).read().rstrip("\n")
+            filePath = "." if file[1] == "" else file[1]
+            # Creating command and adding to the list
+            commandList.append("mkdir -p " + filePath + " && wget -O " + filePath + "/" + fileName + " " + file[0])
+        
+        return commandList
 
     @classmethod
     def runDAQ(cls, protocol, args, outDir=None, clean=True):
@@ -126,8 +156,13 @@ class Plugin(pwem.Plugin):
     @classmethod
     def runEmap2sec(cls, protocol, args, outDir=None, clean=True):
         print("CUSTOM PRINT - start - runEmap2sec") # REMOVE
-        """ Run Emap2sec script from a given protocol. """
-        fullProgram = '%s %s && %s' % (cls.getCondaActivationCmd(), cls.getEnvActivation('EMAP2SEC'), 'python3')
+        """
+        Run Emap2sec script from a given protocol.
+        """
+        fullProgram = "{} {} && {}"\
+            .format(cls.getCondaActivationCmd(),
+                cls.getProtocolActivationCommand('EMAP2SEC'),
+                'python3')
         if not 'main.py' in args:
             args = '{}/main.py '.format(cls._emap2secRepo) + args
         protocol.runJob(fullProgram, args, cwd=cls._emap2secRepo)
@@ -142,24 +177,10 @@ class Plugin(pwem.Plugin):
         print("CUSTOM PRINT - end - runEmap2sec") # REMOVE
 
     @classmethod
-    def getEnviron(cls):
-        print("CUSTOM PRINT - start - getEnviron") # REMOVE
-        print("CUSTOM PRINT - end - getEnviron") # REMOVE
-        pass
-
-    @classmethod
-    def getEnvActivation(cls, protocolName):
-        print("CUSTOM PRINT - getEnvActivation ", protocolName) # REMOVE
-        print("EXAMPLE 1 - ", cls.getVar(protocolName + "_ENV_ACTIVATION")) # REMOVE
-        print("EXAMPLE 2 - ", cls.getVar("DAQ_ENV_ACTIVATION")) # REMOVE
-        return cls.getVar(protocolName + "_ENV_ACTIVATION") # REMOVE
-    
-    @classmethod
     def getGitUrl(cls, protocolName):
         """
-        Returns the GitHub url for the given Kiharalab protocol
+        Returns the GitHub url for the given Kiharalab protocol.
         """
-        print("CUSTOM PRINT - getGitUrl ", protocolName) # REMOVE
         return KIHARALAB_GIT + protocolName
     
     @classmethod
@@ -170,4 +191,3 @@ class Plugin(pwem.Plugin):
         return "conda activate " + getattr(cls, variableName + "_WITH_VERSION")
 
     # ---------------------------------- Utils functions  -----------------------
-
