08ecbe12e2d761f64bc22f5e50102901a1224615
Martin Salinas <martin.salinas@estudiante.uam.es>
2022-05-04 17:47:00 +0200

    Test working now

--- kiharalab/tests/test_emap2sec.py
+++ kiharalab/tests/test_emap2sec.py
@@ -33,28 +33,57 @@ class TestEmap2sec(BaseTest):
     def setUpClass(cls):
         setupTestProject(cls)
         cls.ds = DataSet.getDataSet('model_building_tutorial')
-        cls._runImportVolume()
+
+        # Running test with volume as input
+        cls._runImportVolumes(True)
+
+        # Running test with set of volumes as input
+        cls._runImportVolumes()
 
     @classmethod
-    def _runImportVolume(cls):
-        args = {'filesPath': cls.ds.getFile(
-            'volumes/emd_3488.map'),
+    def _runImportVolumes(cls, single=False):
+        # Creating arguments for import volumes protocol
+        inputPath = 'volumes/1733' + ('' if single else '*') + '.mrc'
+        args = {
+            'filesPath': cls.ds.getFile(inputPath),
             'samplingRate': 1.0,
             'setOrigCoord': False
         }
-        protImportVolume = cls.newProtocol(ProtImportVolumes, **args)
-        cls.launchProtocol(protImportVolume)
-        cls.protImportVolume = protImportVolume
 
-    def _runemap2sec(self):
+        # Creating and launching import volumes protocol
+        protImportVolumes = cls.newProtocol(ProtImportVolumes, **args)
+        cls.launchProtocol(protImportVolumes)
+
+        # Storing results in different variable if input is Volume or SetOfVolumes
+        if single:
+            cls.protImportVolume = protImportVolumes
+        else:
+            cls.protImportVolumes = protImportVolumes
+    
+    def _runemap2sec(self, single=False):
+        # Getting input volumes and defining output variable string
+        inputData = self.protImportVolume.outputVolume if single else self.protImportVolumes.outputVolumes
+        outputVariable = 'outputAtomStruct' + ('' if single else 's')
+
+        # Running protocol
         protEmap2sec = self.newProtocol(
             ProtEmap2sec,
-            inputVolume=self.protImportVolume.outputVolume,
+            inputVolume=inputData,
             contour=2.75)
-
         self.launchProtocol(protEmap2sec)
-        pdbOut = getattr(protEmap2sec, 'outputAtomStructs', None)
+
+        # Checking function output
+        pdbOut = getattr(protEmap2sec, outputVariable, None)
         self.assertIsNotNone(pdbOut)
 
-    def testFpocket(self):
+    def testEmap2sec1(self):
+        """First test. Runs Emap2sec with Volume as input"""
+        print("Running Emap2sec with Volume as input")
+        # Running Emap2se with Volume as input
+        self._runemap2sec(True)
+    
+    def testEmap2sec2(self):
+        """Second test. Runs Emap2sec with SetOfVolumes as input"""
+        print("Running Emap2sec with SetOfVolumes as input")
+        # Running Emap2se with SetOfVolumes as input
         self._runemap2sec()
